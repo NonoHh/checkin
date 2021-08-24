c8b3e64b6f1b79bd2dc6c81aa989a6c8b95c9964
Jiahui <nonohh98.ms@gmail.com>
2021-08-25 00:17:02 +0800

    feat: initialize project with ding talk message push util

---
+++ utils/message.py
@@ -0,0 +1,30 @@
+import base64
+import hashlib
+import hmac
+import json
+import time
+import urllib.parse
+import requests
+import yaml
+
+
+def push_message(msg, is_at_all=False):
+    with open('../config.yaml.sample') as file:
+        config = yaml.safe_load(file)
+        secret = config['DING_SECRET']
+        token = config['DING_TOKEN']
+
+    timestamp = str(round(time.time() * 1000))
+    secret_enc = secret.encode("utf-8")
+    string_to_sign = "{}\n{}".format(timestamp, secret)
+    string_to_sign_enc = string_to_sign.encode("utf-8")
+    hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    send_data = {"at": {"isAtAll": is_at_all}, "msgtype": "text", "text": {"content": msg}}
+    requests.post(
+        url="https://oapi.dingtalk.com/robot/send?access_token={0}&timestamp={1}&sign={2}".format(
+            token, timestamp, sign
+        ),
+        headers={"Content-Type": "application/json", "Charset": "UTF-8"},
+        data=json.dumps(send_data),
+    )
